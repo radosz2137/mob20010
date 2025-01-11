0fddedf15f6ff3c50112bcc80926b5ad7f87d578
radosz2137 <olekradosz2@gmail.com>
2025-01-11 23:47:01 +0100

    Update admin.js

--- xobyz-main/string/js/admin.js
+++ xobyz-main/string/js/admin.js
@@ -1,6 +1,1 @@
-function addApprovedFingerprint(fingerprint) {
-    approvedFingerprints[fingerprint] = true;
-    // Save to storage/file
-    localStorage.setItem('approvedFingerprints', 
-        JSON.stringify(approvedFingerprints));
-}
+
